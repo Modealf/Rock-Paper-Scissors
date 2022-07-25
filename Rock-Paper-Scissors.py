610064ac1777e20d1182dd2d02475565ad3a9bf1
Modealf <Moala057@gmail.com>
2022-07-25 18:49:53 +0300

    Finished Project

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,67 @@
+from asyncio.constants import DEBUG_STACK_DEPTH
+from os import chdir
+from random import random
+from secrets import choice
+from tkinter import messagebox
+from tkinter import *
+
+
+# Window Config
+
+r = Tk()
+r.title('Rock Paper Scissors')
+r.geometry('350x270')
+
+
+
+# Random Outcomes
+
+
+def Rock_choices():
+    mylist = ["Rock", "Paper", "Scissors"]
+    random = choice(mylist)
+    if random == "Rock":
+        rock = messagebox.showwarning("Tie", "Rock! Its a tie.")
+
+    if random == "Paper":
+        rock = messagebox.showerror("Lose", "Paper! You lose.")
+
+    if random == "Scissors":
+        rock = messagebox.showinfo("Win", "Scissors! You win")
+
+def Paper_choices():
+    mylist = ["Rock", "Paper", "Scissors"]
+    random= choice(mylist)
+    if random == "Rock":
+        rock = messagebox.showinfo("Win", "Rock! You win")
+
+    if random == "Paper":
+        rock = messagebox.showwarning("Tie", "Paper! Its a tie")
+
+    if random == "Scissors":
+        rock = messagebox.showerror("Lose", "Scissors! You lose")
+
+def Scissors_choices():
+    mylist = ["Rock", "Paper", "Scissors"]
+    random= choice(mylist)
+    if random == "Rock":
+        rock = messagebox.showerror("Lose", "Rock! You lost")
+
+    if random == "Paper":
+        rock = messagebox.showinfo("Win", "Paper! You win")
+
+    if random == "Scissors":
+        rock = messagebox.showwarning("Tie", "Scissors! Its a tie")
+
+img = PhotoImage(file="Rock Paper Scissors\\Wooden Background.png")
+label = Label(r ,image=img).place(x=-20,y=-20)
+
+#Buttons
+
+rock = Button(r, text="Rock", font=("Arial Rounded MT Bold", 14), width=20, command=Rock_choices, bg="light green").pack(pady=5)
+paper = Button(r, text="Paper", font=("Arial Rounded MT Bold", 14), width=20, command=Paper_choices, bg="light green").pack(pady=5)
+Scissor = Button(r, text="Scissors", font=("Arial Rounded MT Bold", 14), width=20, command=Scissors_choices, bg="light green").pack(pady=5)
+Stop = Button(r, text="Stop Playing", font=("Bold", 14),width=25, command=r.destroy, bg="pink").pack(pady=30, padx=20)
+
+
+r.mainloop()
